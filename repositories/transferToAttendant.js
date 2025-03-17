dd86f7075c45af4e5a15bff238218a02bb97c009
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-17 13:08:51 -0300

    upload

--- repositories/transferToAttendant.js
+++ repositories/transferToAttendant.js
@@ -1,5 +1,4 @@
 const { getCurrentTime } = require('./currentTime');
-const fs = require('fs');
 
 const attendants = [
     { name: 'Carol', number: '5511999999999' },
@@ -13,14 +12,6 @@ const transferToAttendant = (message) => {
     const whatsappLink = `https://wa.me/${attendant.number}`;
     
     console.log(`[${getCurrentTime()}] Cliente transferido para atendente ${attendant.name}.`);
-    
-    fs.appendFile('log.txt', logMessage + '\n', (err) => {
-        if (err) {
-            console.error(`[${getCurrentTime()}] Erro ao gravar no arquivo:`, err);
-        } else {
-            console.log(`[${getCurrentTime()}] Log registrado no arquivo log.txt`);
-        }
-    });
 
     message.reply(`Você será transferido para *${attendant.name}*. Clique no link abaixo para iniciar a conversa:\n${whatsappLink}`);
 };
